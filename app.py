3ff1e92f1a141171adeb86150cee9d83830def31
Karla-Flores <ms.kaflores@gmail.com>
2021-10-04 11:28:58 -0500

    update

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # Import libraries
-# import numpy as np
+import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 
 
-app = Flask(__name__)
-# app = Flask(__name__, template_folder='app')
+# app = Flask(__name__)
+app = Flask(__name__, template_folder='app')
 
 
 # Load the model
@@ -15,7 +15,21 @@ print(model)
 
 @app.route("/")
 def home():
-    return render_template("app/index.html")
+    return render_template("index.html")
+
+@app.route("/api")
+def api_list():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+    )
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    filter = request.form['user-input']
+    # use your pickled model with the user input
+    # to generate a new prediction
+    return "hello world" # really, you'll want to return a template with the predictions
 
 
 if __name__ == "__main__":
