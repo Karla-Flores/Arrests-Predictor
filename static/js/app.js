3c7922c1fe3df2e62169dd3e49f8b4380f91df84
My Name <harika.raj@hotmail.com>
2021-10-06 22:29:45 -0500

    added leaflet code for maps, not working

--- static/js/app.js
+++ static/js/app.js
@@ -26,3 +26,37 @@ d3.select("#model_result").on("click", function(){
     })
 });
 
+// const street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// });
+
+const myMap = L.map("map", {
+    center: [
+        15.6, -28.7
+    ],
+    zoom: 3
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// setTimeout(function() {map.invalidateSize()},1000)
+
+// function initMap() {
+//     const myLatlng = { lat: 25.34512195709302, lng: -89.8890446170271 };
+//     const map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 3,
+//         center: myLatlng,
+//         streetViewControl: false,
+//         mapTypeId: 'terrain',
+//         fullscreenControl: false,
+//         mapTypeControl: false,
+//     });
+//     // Configure the click listener.
+//     map.addListener('click', (mapsMouseEvent) => {
+//         $('#lat').val(mapsMouseEvent.latLng.lat);
+//         $('#lon').val(mapsMouseEvent.latLng.lng);
+//     });
+// }
