e6e7a26e1dd9b616e5985dd8972578f2f9b9c193
My Name <harika.raj@hotmail.com>
2021-10-07 15:36:00 -0500

    tried working on gmaps, not successful

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@
 
 
 
-d3.select("#model_result").on("click", function(){
+d3.select("#model_result").on("click", function () {
     let crime = d3.select("#crime_dropdown").property('value')
     let location = d3.select("#location_dropdown").property('value')
     let season = d3.select("#season_dropdown").property('value')
@@ -14,16 +14,16 @@ d3.select("#model_result").on("click", function(){
     console.log(`127.0.0.1:5000/results/${crime}/${location}/${season}/${hour}/${lat}/${lon}/${domestic}`)
     d3.json(`/results/${crime}/${location}/${season}/${hour}/${lat}/${lon}/${domestic}`)
 
-    .then(function(data){
-        
-        var arrest_prob = (data.prob[0][1]*100).toFixed()
-        document.getElementById('arrest_prob').innerHTML = arrest_prob+" %"
-        // console.log(data)()
-        console.log(data.result)
-        console.log(`probability of arrest is ${(data.prob[0][1]*100).toFixed(2)} %`)
-        
-    
-    })
+        .then(function (data) {
+
+            var arrest_prob = (data.prob[0][1] * 100).toFixed()
+            document.getElementById('arrest_prob').innerHTML = arrest_prob + " %"
+            // console.log(data)()
+            console.log(data.result)
+            console.log(`probability of arrest is ${(data.prob[0][1] * 100).toFixed(2)} %`)
+
+
+        })
 });
 
 
@@ -47,12 +47,12 @@ d3.select("#model_result").on("click", function(){
 //     accessToken: 'REPLACE_WITH_YOUR_TOKEN'
 // }).addTo(mymap);
 
-mapid.on('click', function(e){
-    var coord = e.latlng;
-    var lat = coord.lat;
-    var lng = coord.lng;
-    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
-    });
+// map.on('click', function(e){
+//     var coord = e.latlng;
+//     var lat = coord.lat;
+//     var lng = coord.lng;
+//     console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+//     });
 
 // setTimeout(function() {map.invalidateSize()},1000)
 
@@ -72,3 +72,33 @@ mapid.on('click', function(e){
 //         $('#lon').val(mapsMouseEvent.latLng.lng);
 //     });
 // }
+
+function initMap() {
+    const myLatlng = { lat: -25.363, lng: 131.044 };
+    const map = new google.maps.Map(document.getElementById("mapid"), {
+        zoom: 4,
+        center: myLatlng,
+    });
+    // Create the initial InfoWindow.
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to get Lat/Lng!",
+        position: myLatlng,
+    });
+
+    infoWindow.open(map);
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+    });
+    console.log("Harika")
+}
+
